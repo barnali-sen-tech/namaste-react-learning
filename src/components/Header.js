2aec7a9dfa60f08849801febea95dbee5205f19d
barnali-sen-tech <barnalialpha20@gmail.com>
2023-09-15 00:52:14 +0530

    tailwind css implementation

--- src/components/Header.js
+++ src/components/Header.js
@@ -8,7 +8,7 @@ const Title = () => {
       // <h1>Food Villa</h1>
       <a href="\">
         <img
-          className="logo"
+          className="w-56"
           alt="logo"
           src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTweev2cNZCgecH7BJbsS2WxVkVCOWIDj6M3Q&usqp=CAU"
         />
@@ -18,15 +18,15 @@ const Title = () => {
   export const HeaderComponent = () => {
     console.log("Header render")
     return (
-      <div className="header">
+      <div className="flex justify-between bg-pink-100">
         <Title />
-        <div className="nav-items">
-          <ul>
-            <li><Link to="/">Home</Link></li>
-            <li><Link to="/about">About</Link></li>
-            <li><Link to="/contact">Contact</Link></li>
-            <li><Link to="/grocery">Grocery</Link></li>
-            <li>Cart</li>
+        <div className="flex items-center">
+          <ul className="flex p-4 m-4">
+            <li className="px-4"><Link to="/">Home</Link></li>
+            <li className="px-4"><Link to="/about">About</Link></li>
+            <li className="px-4"><Link to="/contact">Contact</Link></li>
+            <li className="px-4"><Link to="/grocery">Grocery</Link></li>
+            <li className="px-4">Cart</li>
             {/* <button className="login" onClick={()=>{
               btnNameReact==="Login"?setBtnNameReact("Logout"):setBtnNameReact("Login")
             }}>{btnNameReact}</button> */}
